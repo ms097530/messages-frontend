ba36846edbbed750e5ef1bf8a9489d9b4d5647ff
Michael Schultz <mikewschultz9029@gmail.com>
2022-11-13 22:00:28 -0500

    updated domain to use hosted backend

--- components/DomainContext/DomainContext.js
+++ components/DomainContext/DomainContext.js
@@ -1,11 +1,11 @@
 import { useState, createContext } from "react"
 
-export const DomainContext = createContext(process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'http://localhost:8000')
+export const DomainContext = createContext(process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://maple-seemly-visage.glitch.me')
 export const DomainContextDispatch = createContext(undefined)
 
 export function DomainWrapper({ children })
 {
-    const [domain, setDomain] = useState(process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'http://localhost:8000')
+    const [domain, setDomain] = useState(process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://maple-seemly-visage.glitch.me')
     return (
         <DomainContext.Provider value={domain}>
             <DomainContextDispatch.Provider value={setDomain}>
